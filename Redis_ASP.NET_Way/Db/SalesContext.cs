5796e15b212c7e61e4fc99bbb3b8d934421bdd84
temo14 <temo599922030gmail.com>
2023-02-12 11:24:05 -0800

    add context and models

---
+++ Redis_ASP.NET_Way/Db/SalesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Redis_ASP.NET_Way.Models;
+
+namespace Redis_ASP.NET_Way.Db
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext(DbContextOptions<SalesContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+    }
+}
---
+++ Redis_ASP.NET_Way/Models/Sale.cs
@@ -0,0 +1,11 @@
+﻿namespace Redis_ASP.NET_Way.Models
+{
+    public class Sale
+    {
+        public int SaleId { get; set; }
+        public int Total { get; set; }
+
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
