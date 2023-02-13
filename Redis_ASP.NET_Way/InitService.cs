6f00bd12f62bd11907deae0215dd8f09c14fa137
temo14 <temo599922030gmail.com>
2023-02-13 11:03:22 -0800

    add service for clear and fetch cahce

---
+++ Redis_ASP.NET_Way/InitService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Redis_ASP.NET_Way.Db;
+using Redis_ASP.NET_Way.Models;
+
+namespace Redis_ASP.NET_Way
+{
+    public class InitService : IHostedService
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IDistributedCache _cache;
+
+        public InitService(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = _scopeFactory.CreateScope();
+
+            var salesDb = scope.ServiceProvider.GetRequiredService<SalesContext>();
+
+            var cache = scope.ServiceProvider.GetRequiredService<IDistributedCache>();
+            // remove old data
+            var cachePipe = new List<Task>
+            {
+                cache.RefreshAsync("top:sales", cancellationToken),
+                cache.RefreshAsync("top:name", cancellationToken),
+                cache.RefreshAsync("totalSales", cancellationToken)
+            };
+
+            cachePipe.AddRange(salesDb.Employees.Select(employee =>
+                                                  cache.RemoveAsync($"employee:{employee.EmployeeId}:avg", cancellationToken)));
+
+            await Task.WhenAll(cachePipe);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
